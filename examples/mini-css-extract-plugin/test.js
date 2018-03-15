0c49f9d12bd269b972e6233afa6c94d58dc2aefc
Julian Scheid <julians37@gmail.com>
2018-03-15 14:23:50 +0100

    Add failing test case for mini-css-extract-plugin
    Issue #68

---
+++ examples/mini-css-extract-plugin/test.js
@@ -0,0 +1,8 @@
+var webpackVersion = Number(
+  require('webpack/package.json').version.split('.')[0]
+);
+
+module.exports.skip = function skip() {
+  // mini-css-extract-plugin needs Webpack 4
+  return webpackVersion < 4;
+};
