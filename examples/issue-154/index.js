65694515aafd65f59340ecfbe515ffecb61ab866
Julian Scheid <julians37@gmail.com>
2021-03-01 23:56:28 +1300

    Better fix for tests

--- examples/issue-154/index.js
+++ examples/issue-154/index.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   console.log(
     getComputedStyle(document.body).background.match(/rgb\(255, 0, 0\)/)
       ? "ok"
